d3f2cc3f3cfa230aa573b09b96daf7b4658ab050
Азат Ильдарханов <defcon797@gmail.com>
2022-09-17 23:14:38 +0300

    integrate Allure

--- src/test/java/ru/netology/tests/CreditTest.java
+++ src/test/java/ru/netology/tests/CreditTest.java
@@ -1,5 +1,7 @@
 package ru.netology.tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import ru.netology.data.DataHelper;
 import ru.netology.page.PurchasePage;
@@ -7,6 +9,16 @@ import ru.netology.page.PurchasePage;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CreditTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void shouldStart() {
         DataHelper.clearSUTData();
